951943dc425070a74499b772caf70502cc25a8d9
Fumio Nonaka <fumiononaka@FumioNonaka.local>
2019-09-05 18:40:24 +0900

    Vue.js + CLI入門 03: データの計算処理とローカルへの保存

---
+++ src/components/TodoController.vue
@@ -0,0 +1,22 @@
+<template>
+	<footer class="footer" v-show="todos.length" v-cloak>
+		<span class="todo-count">
+			<strong>{{remaining}}</strong> {{remaining | pluralize}} left
+		</span>
+	</footer>
+</template>
+
+<script>
+export default {
+	name: 'TodoController',
+	filters: {
+		pluralize(n) {
+			return n === 1 ? 'item' : 'items';
+		}
+	},
+	props: {
+		todos: Array,
+		remaining: Number
+	}
+}
+</script>
